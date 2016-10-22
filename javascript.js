ed6fd04a8194aae2f59f84a21e4a3d426d76c741
Vncegd <vncentn@aol.com>
2016-10-21 23:51:24 -0400

    Create javascript.js

---
+++ javascript.js
@@ -0,0 +1,31 @@
+//Script derived from w3schools.com/bootstrap/bootstrap_theme_company.asp
+//Creates a smooth scrolling effect when links are clicked.
+$(document).ready(function(){
+  // Add smooth scrolling to all links in navbar + footer link
+  $("a").on('click', function(event) {
+    // Prevent default anchor click behavior
+    event.preventDefault();
+    // Store hash
+    var hash = this.hash;
+    // Using jQuery's animate() method to add smooth page scroll
+    // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
+    $('html, body').animate({
+      scrollTop: $(hash).offset().top
+    }, 500, function(){
+    // Add hash (#) to URL when done scrolling (default click behavior)
+    window.location.hash = hash;
+    });
+  });
+})
+//Fades in elements on scroll.
+$(window).scroll(function() {
+  $(".hiddenStuff").each(function(){
+    var pos = $(this).offset().top;
+
+    var winTop = $(window).scrollTop();
+    if (pos < winTop + 550) {
+      $(this).addClass("visibleStuff");
+    }
+  });
+});
+
